152bae4cd0d13ffe2a57cdb5ba85b2d16d1884e9
Michael C. Toren <mct@toren.net>
2009-12-16 03:52:01 -0800

    Improve the AJAX, so it works with non-javascript browsers, too (like my phone)

--- server/webserver.py
+++ server/webserver.py
@@ -37,7 +37,8 @@ class LightsHandler(BaseHTTPRequestHandler):
 
         for f in sorted(files):
             #self.wfile.write("<li><a href=/%s.seq>%s</a></li>\n" % (f,f) )
-            self.wfile.write("<li><a href='javascript:bgsend(\"%s\")'>%s</a></li>\n" % (f,f) )
+            #self.wfile.write("<li><a href='javascript:bgsend(\"%s\")'>%s</a></li>\n" % (f,f) )
+            self.wfile.write("<li><a href='/%s.seq' onclick='bgsend(\"%s\");return false'>%s</a></li>\n" % (f, f,f) )
 
         footer = open(self.base_path() + "/html/footer.html")
         self.wfile.write(footer.read())
